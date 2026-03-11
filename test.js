4509ceddcba45e7acf596346b6ac33fb3133d20e
athul-aot <athul.ramesh@aot-technologies.com>
2026-03-10 23:16:15 -0700

    chore: Remove obsolete test file
    - Delete test.js containing legacy user group validation logic
    - File contained hardcoded localStorage access and group checking that is no longer needed
    - Cleanup of unused test code to reduce repository clutter

--- test.js
+++
@@ -1,9 +0,0 @@
-const UserDetails = JSON.parse(localStorage.getItem("UserDetails"))
-const groups = UserDetails["groups"]
-if(groups.includes("/DarkEye-Super-User")){
-  show = true;
-}
-else{
-  show = false;
-};
-
